f03d24308ec2815f3397aeb27089ee8a5e5f61d8
Nick McClorey <mcclorns@mail.uc.edu>
2018-03-30 18:17:37 -0400

    first commit

---
+++ chessboard.cpp
@@ -0,0 +1,12 @@
+#include "chess_piece.h"
+#include "chessboard.h"
+
+using namespace std;
+
+int Chessboard::function1() {
+	return 3;
+}
+
+Chessboard::Chessboard() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include "chess_piece.h"
+#include "chessboard.h"
+
+using namespace std;
+
+int main() {
+
+	Chessboard num1;
+	
+
+	cout << "Working" << endl;
+
+	system("pause");
+	return 0;
+}
