d6298f3de5a6cc66a26738d971048e9f4daf6860
Nick McClorey <mcclorns@mail.uc.edu>
2018-04-10 15:59:54 -0400

    found the bug

--- Chessboard.cpp
+++ Chessboard.cpp
@@ -21,47 +21,48 @@ void Chessboard::newGame() {
 		board[6][i].setType("pawn");
 		board[6][i].setTeam("white");
 	}
-}
 
-	/*
+
+	
     for (int i = 0; i < 8; i++) {
-        switch (i)
-            case 0:
-                board[0][i].setType("rook");
-                board[7][i].setType("rook");
-                break;
-            case 1:
-                board[0][i].setType("knight");
-                board[7][i].setType("knight");
-                break;
-            case 2:
-                board[0][i].setType("bishop");
-                board[7][i].setType("bishop");
-                break;
-            case 3:
-                board[0][i].setType("queen");
-                board[7][i].setType("queen");
-                break;
-            case 4:
-                board[0][i].setType("king");
-                board[7][i].setType("king");
-                break;
-            case 5:
-                board[0][i].setType("bishop");
-                board[7][i].setType("bishop");
-                break;
-            case 6:
-                board[0][i].setType("knight");
-                board[7][i].setType("knight");
-                break;
-            case 7:
-                board[0][i].setType("rook");
-                board[7][i].setType("rook");
-                break;
-        
-        board[0][i].setTeam("black");
-        board[7][i].setTeam("white");
-    }
+		switch (i) {
+		case 0:
+			board[0][i].setType("rook");
+			board[7][i].setType("rook");
+			break;
+		case 1:
+			board[0][i].setType("knight");
+			board[7][i].setType("knight");
+			break;
+		case 2:
+			board[0][i].setType("bishop");
+			board[7][i].setType("bishop");
+			break;
+		case 3:
+			board[0][i].setType("queen");
+			board[7][i].setType("queen");
+			break;
+		case 4:
+			board[0][i].setType("king");
+			board[7][i].setType("king");
+			break;
+		case 5:
+			board[0][i].setType("bishop");
+			board[7][i].setType("bishop");
+			break;
+		case 6:
+			board[0][i].setType("knight");
+			board[7][i].setType("knight");
+			break;
+		case 7:
+			board[0][i].setType("rook");
+			board[7][i].setType("rook");
+			break;
+
+			board[0][i].setTeam("black");
+			board[7][i].setTeam("white");
+		}
+	}
     
 }
 
@@ -72,4 +73,3 @@ void Chessboard::newGame() {
 
 
 
-*/
